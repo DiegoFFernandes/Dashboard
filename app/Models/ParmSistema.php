d1c9152fb700e3c6ccdc6cfac6185931602be85d
DiegoFFernandes <diego.ffun4@hotmail.com>
2024-09-02 15:49:02 -0300

    Aumento de licença

--- app/Models/ParmSistema.php
+++ app/Models/ParmSistema.php
@@ -11,7 +11,7 @@ class ParmSistema extends Model
     use HasFactory;
 
     public function UpdateLicencaColeta(){
-        $query = 'update parmsistema p set p.nrlicencacoleta = 135';
+        $query = 'update parmsistema p set p.nrlicencacoleta = 155';
         DB::connection('firebird_campina')->select($query);
         return DB::connection('firebird_paranavai')->select($query);
     }
